32b894bc5926120ac069d0ee22d5f38cf69d907c
Maria Luiza Dornelas <mariadornelasc12@gmail.com>
2026-09-23 16:15:47 -0300

    Ajusta teste sensor

--- src/test/java/com/faculdade/sensor/SensorFactoryTest.java
+++ src/test/java/com/faculdade/sensor/SensorFactoryTest.java
@@ -4,16 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Testa a {@link SensorFactory}, que agora acumula dois papéis:
- * <ul>
- *   <li><b>Factory Method</b>: {@code obterSensor(tipo)} encapsula a
- *       criação do produto concreto e devolve sempre o tipo abstrato
- *       {@link LeituraSensor};</li>
- *   <li><b>Singleton</b>: {@code getInstance()} sempre retorna a mesma
- *       instância da fábrica.</li>
- * </ul>
- */
 class SensorFactoryTest {
 
     @Test
@@ -21,8 +11,6 @@ class SensorFactoryTest {
         SensorFactory instancia1 = SensorFactory.getInstance();
         SensorFactory instancia2 = SensorFactory.getInstance();
 
-        // Prova central do padrão Singleton: as duas variáveis apontam
-        // para o mesmo objeto na memória.
         assertSame(instancia1, instancia2, "getInstance() deveria sempre retornar a mesma instância");
     }
 
@@ -50,8 +38,7 @@ class SensorFactoryTest {
 
     @Test
     void obterSensorDeveRetornarInstanciasDiferentesACadaChamada() {
-        // O Singleton é a FÁBRICA, não o produto: cada chamada a
-        // obterSensor() ainda deve criar um novo objeto LeituraSensor.
+
         LeituraSensor sensor1 = SensorFactory.getInstance().obterSensor("Temperatura");
         LeituraSensor sensor2 = SensorFactory.getInstance().obterSensor("Temperatura");
 
